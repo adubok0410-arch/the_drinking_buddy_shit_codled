9047dbb00819da36d29a665ef1fac957dfd921ad
Aleksey <a.dubok.0410@gmail.com>
2026-01-25 18:03:49 +0300

    добавлено окончание

---
+++ dz/25.01.26.py
@@ -0,0 +1,49 @@
+# №892
+
+# file_in = open("INPUT.txt", "r", encoding="utf-8")
+# file_out = open("OUTPUT.txt", "w", encoding="utf-8")
+
+# month = int(file_in.readline())
+
+# file_in.close()
+
+# if month in [1, 2, 12]:
+#     file_out.write("Winter")
+# elif month in [3, 4, 5]:
+#     file_out.write("Spring")
+# elif month in [6, 7, 8]:
+#     file_out.write("Summer")
+# elif month in [9, 10, 11]:
+#     file_out.write("Autumn")
+# else:
+#     file_out.write("Error")
+
+# file_out.close()
+
+# ЗАДАЧА №23
+
+# file_in = open("INPUT.txt", "r", encoding="utf-8")
+# file_out = open("OUTPUT.txt", "w", encoding="utf-8")
+
+# number = int(file_in.readline)
+
+# file_in.close()
+
+# def natural_divisord(number):
+
+#     arr = []
+
+#     for i in range(1, int(number ** 0.5) + 1):
+
+#             if number % i == 0:
+#                   arr.append(i)
+#                   if number // i != i:
+#                         arr.append(number // i)
+
+#     return sum(arr)
+
+# file_out.write(natural_dividors(number))
+
+# file_out.close()
+
+
